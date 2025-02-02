4e916c344dd40c76b15f17b99d0d353a559d2e72
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 22:36:33 +0000

    feat: add listing_price to mrt__top_performing_products

--- models/mart/mrt__top_performing_products_per_store_category.sql
+++ models/mart/mrt__top_performing_products_per_store_category.sql
@@ -9,9 +9,10 @@ WITH product_purchases_per_store_category AS (
         store_id,
         category_name,
         product_name,
+        price_dollars,
         sum(quantity) as total_items_sold
     FROM {{ ref("int__order_items_enriched") }}
-    GROUP BY 1,2,3
+    GROUP BY 1,2,3, 4
     QUALIFY ROW_NUMBER() OVER (
         PARTITION BY store_id, category_name ORDER BY SUM(quantity) DESC
         ) <= 3
