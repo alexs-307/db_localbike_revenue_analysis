66164787674ba4e01a0b55c1335856f2a6895457
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 15:28:58 +0000

    fix: int__order_financial_metrics

--- models/intermediate/int__order_financial_metrics.sql
+++ models/intermediate/int__order_financial_metrics.sql
@@ -4,3 +4,4 @@ select
     SUM(quantity * price_dollars) as total_order_value_wo_discounts_dollars,
     SUM(quantity * price_dollars * discount_pct) as total_discounts_dollars
 from {{ ref("int__order_items_enriched") }}
+group by order_id
