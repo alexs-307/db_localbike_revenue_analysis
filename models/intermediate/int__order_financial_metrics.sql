0521916c93e1d113706f089ca5583119e6dd789b
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 15:53:38 +0000

    fix: group by in int__order_financial_metrics.sql

--- models/intermediate/int__order_financial_metrics.sql
+++ models/intermediate/int__order_financial_metrics.sql
@@ -2,6 +2,7 @@ select
     order_id,
     staff_id,
     store_id,
+    days_between_order_and_shipping,
     COUNT(DISTINCT product_id) as cnt_distinct_products,
     COUNT(product_id) as cnt_products,
     COUNT(category_id) as cnt_categories,
@@ -12,4 +13,4 @@ select
     SUM(quantity * price_dollars * discount_pct) as total_discounts_dollars,
     1.* SUM(CASE WHEN has_discount = 1 THEN quantity END) / SUM(quantity) as pct_items_sold_discounted
 from {{ ref("int__order_items_enriched") }}
-group by order_id
+group by order_id, staff_id, store_id, days_between_order_and_shipping
