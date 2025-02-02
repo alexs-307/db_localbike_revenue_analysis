5a1772f02fb36c72051a053f6d9b5a8b1c883f68
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 18:01:26 +0000

    feat: adding cnt_sellers_with_sale in daily_revenues_per_store

--- models/mart/daily_revenues_per_store.sql
+++ models/mart/daily_revenues_per_store.sql
@@ -5,6 +5,7 @@ select
     sum(total_order_value_wo_discounts_dollars) as total_revenue_pre_discount_dollars,
     count(distinct order_id) as cnt_orders,
     avg(total_order_value_dollars) as avg_basket_dollars,
-    avg(total_item_quantity) as avg_items_per_basket
+    avg(total_item_quantity) as avg_items_per_basket,
+    count(distinct seller_id) as cnt_sellers_with_sale
 from {{ ref("int__order_financial_metrics") }}
 group by dt, store_id
