b4bf260ed06a2d35ec9f8dd965b583e44a4fbf94
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 21:45:03 +0000

    feat: add breakdown by category_id to int__order_financial_metrics

--- models/intermediate/int__order_financial_metrics.sql
+++ models/intermediate/int__order_financial_metrics.sql
@@ -2,15 +2,15 @@ select
     order_id,
     staff_id,
     store_id,
+    category_id,
     order_date,
     COUNT(DISTINCT product_id) as cnt_distinct_products,
     COUNT(product_id) as cnt_products,
-    COUNT(category_id) as cnt_categories,
     COUNT(DISTINCT category_id) as cnt_distinct_categories,
     SUM(quantity) as total_item_quantity,
     SUM(quantity * price_dollars * (1-discount_pct)) as total_order_value_dollars,
     SUM(quantity * price_dollars) as total_order_value_wo_discounts_dollars,
     SUM(quantity * price_dollars * discount_pct) as total_discounts_dollars,
-    1.* SUM(CASE WHEN has_discount = 1 THEN quantity END) / SUM(quantity) as pct_items_sold_discounted
+    SUM(CASE WHEN has_discount = 1 THEN quantity END) as cnt_items_sold_discounted
 from {{ ref("int__order_items_enriched") }}
-group by order_id, staff_id, store_id, order_date
+group by order_id, staff_id, store_id, category_id, order_date
