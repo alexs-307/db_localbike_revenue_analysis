7dd652faa05d0d647047f80f4205b7a80a3b49a4
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 22:27:21 +0000

    feat: add top performing product per store and category

---
+++ models/mart/mrt__top_performing_products_per_store_category.sql
@@ -0,0 +1,14 @@
+WITH product_purchases_per_store_category AS (
+    SELECT 
+        store_id,
+        category_name,
+        product_name,
+        sum(quantity) as total_items_sold
+    FROM {{ ref("int__order_items_enriched") }}
+    GROUP BY 1,2,3
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY store_id, category_name ORDER BY SUM(quantity) DESC
+        ) <= 3
+)
+
+select * from product_purchases_per_store_category
