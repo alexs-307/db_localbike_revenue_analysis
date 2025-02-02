78d8445b2fa96910c1760be1fe743b9567b9208e
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 15:27:28 +0000

    adding order_financial_metrics

---
+++ models/intermediate/int__order_financial_metrics.sql
@@ -0,0 +1,6 @@
+select 
+    order_id,
+    SUM(quantity * price_dollars * (1-discount_pct)) as total_order_value_dollars,
+    SUM(quantity * price_dollars) as total_order_value_wo_discounts_dollars,
+    SUM(quantity * price_dollars * discount_pct) as total_discounts_dollars
+from {{ ref("int__order_items_enriched") }}
--- models/staging/stg__order_items.sql
+++ models/staging/stg__order_items.sql
@@ -5,5 +5,6 @@ select
     product_id,
     quantity,
     list_price as price_dollars,
-    discount as discount_pct
+    discount as discount_pct,
+    CASE WHEN discount > 0 THEN 1 ELSE 0 END has_discount 
 from {{ source("databird_final_project",'order_items') }}
