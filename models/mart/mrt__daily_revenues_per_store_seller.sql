48bc2dafe7082a7db493eef48f8c6dd4e31e9fd9
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 19:34:29 +0000

    feat: add total items per store seller being sold

--- models/mart/mrt__daily_revenues_per_store_seller.sql
+++ models/mart/mrt__daily_revenues_per_store_seller.sql
@@ -12,6 +12,7 @@ select
     sum(total_order_value_dollars) as total_revenue_dollars,
     sum(total_order_value_wo_discounts_dollars) as total_revenue_pre_discount_dollars,
     count(distinct order_id) as cnt_orders,
+    sum(total_item_quantity) as total_items_sold,
     avg(total_order_value_dollars) as avg_basket_dollars_per_seller,
     avg(total_item_quantity) as avg_items_per_basket_per_seller
 from {{ ref("int__order_financial_metrics") }}
