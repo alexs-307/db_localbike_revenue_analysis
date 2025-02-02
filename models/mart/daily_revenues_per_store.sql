a64796d20aeb880e48a97c8195c4a75aaa8c5947
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 17:53:26 +0000

    feat: add basket size metrics per store to mart

--- models/mart/daily_revenues_per_store.sql
+++ models/mart/daily_revenues_per_store.sql
@@ -2,6 +2,9 @@ select
     order_date as dt,
     store_id,
     sum(total_order_value_dollars) as total_revenue_dollars,
-    sum(total_order_value_wo_discounts_dollars) as total_revenue_pre_discount_dollars
+    sum(total_order_value_wo_discounts_dollars) as total_revenue_pre_discount_dollars,
+    count(distinct order_id) as cnt_orders,
+    avg(total_order_value_dollars) as avg_basket_dollars,
+    avg(total_item_quantity) as avg_items_per_basket
 from {{ ref("int__order_financial_metrics") }}
 group by dt, store_id
