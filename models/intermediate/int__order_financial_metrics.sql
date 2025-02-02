4f07ca4411f5b6e470034bff4649453c772eed6b
Yardo Sale <yardosale2023@yahoo.com>
2025-02-02 15:37:37 +0000

    feat: pct_items_sold_discounted added in int__order_financial_metrics

--- models/intermediate/int__order_financial_metrics.sql
+++ models/intermediate/int__order_financial_metrics.sql
@@ -7,6 +7,7 @@ select
     SUM(quantity) as total_item_quantity,
     SUM(quantity * price_dollars * (1-discount_pct)) as total_order_value_dollars,
     SUM(quantity * price_dollars) as total_order_value_wo_discounts_dollars,
-    SUM(quantity * price_dollars * discount_pct) as total_discounts_dollars
+    SUM(quantity * price_dollars * discount_pct) as total_discounts_dollars,
+    1.* SUM(CASE WHEN has_discount = 1 THEN quantity END) / SUM(quantity) as pct_items_sold_discounted
 from {{ ref("int__order_items_enriched") }}
 group by order_id
